e7c6bd4f2295e0a14a8765915d7c4f770e951ef6
3xiced <3xiced@gmail.com>
2022-09-21 15:15:05 +0300

    Lab-1

---
+++ Palindrome.java
@@ -0,0 +1,34 @@
+/**
+ * Class checks if word is a palidrome
+ */
+public class Palindrome {
+    /**
+     * Main method
+     * @param args String array
+     */
+    public static void main(String[] args) {
+        for (String s : args) {
+            System.out.println(isPalindrome(s));
+        }
+    }
+
+    /**
+     * Reverses string
+     * @param str String
+     * @return String
+     */
+    public static String reverseString(String str){
+        StringBuilder sb=new StringBuilder(str);
+        sb.reverse();
+        return sb.toString();
+    }
+
+    /**
+     * Checks if word is a palindrome
+     * @param str String
+     * @return boolean
+     */
+    public static boolean isPalindrome(String str){
+        return str.equals(reverseString(str));
+    }
+}
---
+++ Primes.java
@@ -0,0 +1,35 @@
+/**
+ * Class that checks first 100 numbers to be a prime number
+ */
+public class Primes {
+
+    /**
+     * Main method
+     * @param args
+     */
+    public static void main(String[] args) {
+        for (int i = 2; i < 100; i++) {
+            if (isPrime(i)){
+                System.out.println(i);
+            }
+        }
+    }
+
+    /**
+     * Checks if number is prime
+     * @param num String
+     * @return boolean
+     */
+    public static boolean isPrime(int num) {
+        if(num<=1)
+        {
+            return false;
+        }
+        for(int i=2;i<=num/2;i++)
+        {
+            if((num%i)==0)
+                return  false;
+        }
+        return true;
+    }
+}
